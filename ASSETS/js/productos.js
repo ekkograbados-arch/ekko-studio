66285951c8502908466a36703c416d4ece8715cc
ekkograbados-arch <ekkograbados@gmail.com>
2026-06-20 17:34:08 -0300

    Create productos.js

---
+++ ASSETS/js/productos.js
@@ -0,0 +1,22 @@
+const PRODUCTOS = {
+  medallas: [
+    {
+      nombre:"Medalla Militar 25x45",
+      svg:"mockups-medidas/medalla-militar-25x45.svg"
+    },
+    {
+      nombre:"Medalla Militar 29x50",
+      svg:"mockups-medidas/medalla-militar-29x50.svg"
+    },
+    {
+      nombre:"Medalla Redonda 25mm",
+      svg:"mockups-medidas/medalla-redondo-25mm.svg"
+    }
+  ],
+
+  chapitas:[...],
+
+  pulseras:[...],
+
+  mates:[...]
+}
