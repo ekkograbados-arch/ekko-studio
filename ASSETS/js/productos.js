e47eb6c7e64b27a283486b9f8e4900b8d8958382
ekkograbados-arch <ekkograbados@gmail.com>
2026-06-20 17:53:05 -0300

    Update productos.js

--- ASSETS/js/productos.js
+++ ASSETS/js/productos.js
@@ -1,22 +1,182 @@
-const PRODUCTOS = {
-  medallas: [
-    {
-      nombre:"Medalla Militar 25x45",
-      svg:"mockups-medidas/medalla-militar-25x45.svg"
-    },
-    {
-      nombre:"Medalla Militar 29x50",
-      svg:"mockups-medidas/medalla-militar-29x50.svg"
-    },
-    {
-      nombre:"Medalla Redonda 25mm",
-      svg:"mockups-medidas/medalla-redondo-25mm.svg"
-    }
-  ],
-
-  chapitas:[...],
-
-  pulseras:[...],
-
-  mates:[...]
-}
+window.EKKO_STUDIO_PRODUCTS = [
+  {
+    categoria: "Medallas",
+    productos: [
+      {
+        id: "medalla-militar-25x45",
+        nombre: "Medalla Militar 25x45",
+        svgBase: "maquetas-medidas/medalla-militar-25x45.svg",
+        superficies: [
+          {
+            nombre: "Frente",
+            svg: "maquetas-medidas/medalla-militar-25x45.svg",
+            area: "silueta"
+          },
+          {
+            nombre: "Dorso",
+            svg: "maquetas-medidas/medalla-militar-25x45.svg",
+            area: "silueta"
+          }
+        ]
+      },
+      {
+        id: "medalla-militar-29x50",
+        nombre: "Medalla Militar 29x50",
+        svgBase: "maquetas-medidas/medalla-militar-29x50.svg",
+        superficies: [
+          {
+            nombre: "Frente",
+            svg: "maquetas-medidas/medalla-militar-29x50.svg",
+            area: "silueta"
+          },
+          {
+            nombre: "Dorso",
+            svg: "maquetas-medidas/medalla-militar-29x50.svg",
+            area: "silueta"
+          }
+        ]
+      },
+      {
+        id: "medalla-redonda-25mm",
+        nombre: "Medalla Redonda 25mm",
+        svgBase: "maquetas-medidas/medalla-redonda-25mm.svg",
+        superficies: [
+          {
+            nombre: "Frente",
+            svg: "maquetas-medidas/medalla-redonda-25mm.svg",
+            area: "silueta"
+          },
+          {
+            nombre: "Dorso",
+            svg: "maquetas-medidas/medalla-redonda-25mm.svg",
+            area: "silueta"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    categoria: "Chapitas",
+    productos: [
+      {
+        id: "chapita-huesito-16x32",
+        nombre: "Chapita Huesito 16x32",
+        svgBase: "maquetas-medidas/chapita-huesito-16x32.svg",
+        superficies: [
+          {
+            nombre: "Frente",
+            svg: "maquetas-medidas/chapita-huesito-16x32.svg",
+            area: "silueta"
+          },
+          {
+            nombre: "Dorso",
+            svg: "maquetas-medidas/chapita-huesito-16x32.svg",
+            area: "silueta"
+          }
+        ]
+      },
+      {
+        id: "chapita-huesito-21x40",
+        nombre: "Chapita Huesito 21x40",
+        svgBase: "maquetas-medidas/chapita-huesito-21x40.svg",
+        superficies: [
+          {
+            nombre: "Frente",
+            svg: "maquetas-medidas/chapita-huesito-21x40.svg",
+            area: "silueta"
+          },
+          {
+            nombre: "Dorso",
+            svg: "maquetas-medidas/chapita-huesito-21x40.svg",
+            area: "silueta"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    categoria: "Pulseras",
+    productos: [
+      {
+        id: "pulsera-chica-5x30",
+        nombre: "Pulsera Chica 5x30",
+        svgBase: "maquetas-medidas/pulsera-chica-5x30.svg",
+        superficies: [
+          {
+            nombre: "Frente",
+            svg: "maquetas-medidas/pulsera-chica-5x30.svg",
+            area: "silueta"
+          },
+          {
+            nombre: "Dorso",
+            svg: "maquetas-medidas/pulsera-chica-5x30.svg",
+            area: "silueta"
+          }
+        ]
+      },
+      {
+        id: "pulsera-grande-6x35",
+        nombre: "Pulsera Grande 6x35",
+        svgBase: "maquetas-medidas/pulsera-grande-6x35.svg",
+        superficies: [
+          {
+            nombre: "Frente",
+            svg: "maquetas-medidas/pulsera-grande-6x35.svg",
+            area: "silueta"
+          },
+          {
+            nombre: "Dorso",
+            svg: "maquetas-medidas/pulsera-grande-6x35.svg",
+            area: "silueta"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    categoria: "Mates",
+    productos: [
+      {
+        id: "mate-acero",
+        nombre: "Mate Acero",
+        svgBase: "maquetas-medidas/mate-acero.svg",
+        superficies: [
+          {
+            nombre: "Mate",
+            svg: "maquetas-medidas/mate-acero.svg",
+            area: "rectangulo"
+          }
+        ]
+      },
+      {
+        id: "mate-algarrobo-sin-virola",
+        nombre: "Mate Algarrobo Sin Virola",
+        svgBase: "maquetas-medidas/mate-algarrobo-sin-virola.svg",
+        superficies: [
+          {
+            nombre: "Mate",
+            svg: "maquetas-medidas/mate-algarrobo-sin-virola.svg",
+            area: "rectangulo"
+          }
+        ]
+      },
+      {
+        id: "mate-algarrobo-con-virola",
+        nombre: "Mate Algarrobo Con Virola",
+        svgBase: "maquetas-medidas/mate-de-algarrobo-con-virola.svg",
+        superficies: [
+          {
+            nombre: "Mate",
+            svg: "maquetas-medidas/mate-de-algarrobo-con-virola.svg",
+            area: "rectangulo"
+          },
+          {
+            nombre: "Virola",
+            svg: "maquetas-medidas/virola-mate-de-algarrobo-con-virola.svg",
+            area: "anillo"
+          }
+        ]
+      }
+    ]
+  }
+];
