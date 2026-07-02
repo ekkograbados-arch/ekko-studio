1162f749b7e5cea7efe70f31143073e34b02e65a
ekkograbados-arch <ekkograbados@gmail.com>
2026-07-02 17:07:54 -0300

    Update selection.js

--- ASSETS/js/modules/selection.js
+++ ASSETS/js/modules/selection.js
@@ -1,1 +1,32 @@
 
+window.selectedItem = null;
+
+window.dragOffset = new paper.Point(0, 0);
+
+window.selectItem = function(item){
+
+    if(window.selectedItem){
+        window.selectedItem.selected = false;
+    }
+
+    window.selectedItem = item;
+
+    if(item){
+        item.selected = true;
+    }
+
+    paper.view.update();
+
+};
+
+window.deselectItem = function(){
+
+    if(window.selectedItem){
+        window.selectedItem.selected = false;
+    }
+
+    window.selectedItem = null;
+
+    paper.view.update();
+
+};
