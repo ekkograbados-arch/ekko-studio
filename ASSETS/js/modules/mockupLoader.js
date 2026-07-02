e1f5e4b0dd639d15263e95d1797d316f176829fd
ekkograbados-arch <ekkograbados@gmail.com>
2026-07-02 10:23:25 -0300

    Create mockupLoader.js

---
+++ ASSETS/js/modules/mockupLoader.js
@@ -0,0 +1,46 @@
+export function loadMockup(svgPath) {
+
+    const token = ++window.loadToken;
+
+    paper.project.activeLayer.removeChildren();
+
+    paper.project.importSVG(svgPath, (item) => {
+
+        if (token !== window.loadToken) {
+
+            if (item) item.remove();
+
+            return;
+
+        }
+
+        if (!item) return;
+
+        const bounds = item.bounds;
+        const canvasBounds = paper.view.bounds;
+
+        const scaleX =
+            (canvasBounds.width * 0.75) / bounds.width;
+
+        const scaleY =
+            (canvasBounds.height * 0.75) / bounds.height;
+
+        const scale = Math.min(scaleX, scaleY);
+
+        item.scale(scale);
+
+        item.position = canvasBounds.center;
+
+        item.data = item.data || {};
+
+        item.data.locked = true;
+
+        item.data.label = "Mockup";
+
+        item.bringToFront();
+
+        paper.view.update();
+
+    });
+
+}
