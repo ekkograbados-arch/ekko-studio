299af8b3c3a1c9b14c22a7907114ce218374e083
ekkograbados-arch <ekkograbados@gmail.com>
2026-08-19 13:14:43 -0300

    Fix import paths in contextualMenu.js

--- ASSETS/js/modules/canvas-pro/contextualMenu.js
+++ ASSETS/js/modules/canvas-pro/contextualMenu.js
@@ -13,7 +13,7 @@ import {
   applyTextCurve, 
   applyTextSpacing,
   loadDynamicFonts 
-} from "/js/modules/canvas-pro/textToolbar.js";
+} from "/ASSETS/js/modules/canvas-pro/textToolbar.js";
 
 import {
   scaleImage,
@@ -21,7 +21,7 @@ import {
   deleteImage,
   bringImageForward,
   sendImageBackward
-} from "/js/modules/canvas-pro/selection.js";
+} from "/ASSETS/js/modules/canvas-pro/selection.js";
 
 // Variable global de previsualización en window
 window.originalFontBackup = null;
